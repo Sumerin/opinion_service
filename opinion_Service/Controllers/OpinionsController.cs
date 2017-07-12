b32406713c0047057aedbd3bdf090a66ff13d779
Sumerin <Sumekpl@gmail.com>
2017-07-12 12:25:19 +0200

    Database update controller reworked

--- opinion_Service/Controllers/OpinionsController.cs
+++ opinion_Service/Controllers/OpinionsController.cs
@@ -18,7 +18,7 @@ namespace opinion_Service.Controllers
         {
             using (var ctx = new MyDbContext())
             {
-                return View(ctx.Opinions.Include("User").ToList());
+                return View(ctx.Opinions.Include("User").Include("Site").ToList());
             }
         }
 
@@ -26,6 +26,7 @@ namespace opinion_Service.Controllers
         // GET: Opinions/Create
         public ActionResult Create()
         {
+            ViewBag.Result = "";
             return View();
         }
 
@@ -34,22 +35,42 @@ namespace opinion_Service.Controllers
         // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "Ip,Description")] Opinion opinion)
+        public ActionResult Create([Bind(Include = "Site, Description")]Opinion opinion)
         {
             if(Session["User"]!=null)
             {
                 User sessionUser = (User)Session["User"];
                 using (var ctx = new MyDbContext())
                 {
-                    var user = from us in ctx.UserAccount
-                               where us.UserId == sessionUser.UserId
-                               select us;
+                    var user = (from us in ctx.UserAccount
+                                where us.UserId == sessionUser.UserId
+                                select us).FirstOrDefault();
+
+                    if(user==null)
+                    {
+                        ViewBag.Result = "Wrong UserName!!";
+                        return View(opinion);
+                    }
+
+                    var site = (from si in ctx.Sites
+                               where si.DomainName == opinion.Site.DomainName
+                               select si).FirstOrDefault();
+
+                    if (site == null)
+                    {
+                        ViewBag.Result = "Site does not exist in database!!";
+                        return View(opinion);
+                    }
+                    opinion.User = user;
+                    opinion.Site = site;
+
                     ctx.Opinions.Add(opinion);
                     ctx.SaveChanges();
                 }
                 return RedirectToAction("Index");
             }
-            return View(opinion);
+            ViewBag.Result = "Login Before adding opinion";
+            return RedirectToAction("Login","Account");
         }
     }
 }
