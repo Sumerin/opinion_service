427f8e7333aa10cf3a91648281a9aade79c1c0d5
Sumerin <Sumekpl@gmail.com>
2017-07-16 22:06:26 +0200

    Get database and view it

--- Administration_Panel/MainWindow.xaml.cs
+++ Administration_Panel/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using opinion_Service.Models;
 
 namespace Administration_Panel
 {
@@ -23,6 +24,50 @@ namespace Administration_Panel
         public MainWindow()
         {
             InitializeComponent();
+
+            SiteDataGrid.AutoGenerateColumns = false;
+            SiteDataGrid.CanUserAddRows = false;
+            SiteDataGrid.CanUserDeleteRows = false;
+
+            LoadSiteToView();
+
+
+            UserDataGrid.AutoGenerateColumns = false;
+            UserDataGrid.CanUserAddRows = false;
+            UserDataGrid.CanUserDeleteRows = false;
+
+            LoadUserDataToView();
+
+
+            OpinionDataGrid.AutoGenerateColumns = false;
+            OpinionDataGrid.CanUserAddRows = false;
+            OpinionDataGrid.CanUserDeleteRows = false;
+
+            LoadOpinionToView();
+            
+        }
+
+        private void LoadSiteToView()
+        {
+            using (var ctx = new MyDbContext())
+            {
+                SiteDataGrid.ItemsSource = ctx.Sites.ToList();
+            }
+        }
+
+        private void LoadUserDataToView()
+        {
+            using (var ctx = new MyDbContext())
+            {
+                UserDataGrid.ItemsSource = ctx.UserAccount.ToList();
+            }
+        }
+        private void LoadOpinionToView()
+        {
+            using (var ctx = new MyDbContext())
+            {
+                OpinionDataGrid.ItemsSource = ctx.Opinions.Include("Site").ToList();
+            }
         }
     }
 }
